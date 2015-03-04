de4b2571b8d697a94e302e35a36e9d8512175242
Amy Michelle Johnson and Debbie Cravey <cinemaestro@yahoo.com>
2015-03-04 10:57:23 -0800

    add model for storing and listing lots

--- controllers/lots.js
+++ controllers/lots.js
@@ -1,12 +1,14 @@
-Auction.LotsController = Ember.Controller.extend({
+Auction.LotsController = Ember.ArrayController.extend({
   actions: {
     save: function() {
+      console.log("here")
       var newLot = this.store.createRecord('lot', {
         title: this.get('title'),
         date: this.get('date')
       });
 
       newLot.save();
+
     }
   }
 });
---
+++ models/lot.js
@@ -0,0 +1,4 @@
+Auction.Lot = DS.Model.extend({
+  title: DS.attr(),
+  date: DS.attr()
+});
