4eab3a42bc06e971afd155d9daa0e68bea08529e
Amy Michelle Johnson and Debbie Cravey <cinemaestro@yahoo.com>
2015-03-04 16:51:24 -0800

    add delete buttons

--- controllers/lot.js
+++ controllers/lot.js
@@ -13,12 +13,20 @@ Auction.LotController = Ember.ObjectController.extend({
         photo: this.get('photo')
       });
       product.save();
-      console.log(product)
-
 
       var lot = this.get('controllers.lot.model');
       lot.get('products').pushObject(product);
       lot.save();
+    },
+
+    delete: function() {
+      if (confirm("Are you sure?")) {
+        console.log ("got here")
+        console.log(this.get('model'))
+      this.get('product').destroyRecord();
+
+      // lot.get('products').destroyRecord();
+      }
     }
   }
 
--- controllers/lots.js
+++ controllers/lots.js
@@ -1,16 +1,22 @@
 Auction.LotsController = Ember.ArrayController.extend({
   actions: {
     save: function() {
-      console.log("here")
       var newLot = this.store.createRecord('lot', {
         title: this.get('title'),
         date: this.get('date')
       });
-
       newLot.save();
 
       this.set('title', null),
       this.set('date', null)
+    },
+
+    delete: function() {
+      if (confirm("Are you sure?")) {
+        console.log ("got here")
+        console.log(this.get('model'))
+      this.get('model').destroyRecord();
+      }
     }
   }
 });
