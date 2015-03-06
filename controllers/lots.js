61f4a6a9528b553e3423ca650dd3670f2f17d705
Debbie <cinemaestro@yahoo.com>
2015-03-06 08:27:35 -0800

    refactor to correct lot photo persistence

--- controllers/lots.js
+++ controllers/lots.js
@@ -4,12 +4,13 @@ Auction.LotsController = Ember.ArrayController.extend({
       var newLot = this.store.createRecord('lot', {
         title: this.get('title'),
         date: this.get('date'),
-        photo: this.get('photo')
+        lotphoto: this.get('lotphoto')
       });
       newLot.save();
 
       this.set('title', null),
-      this.set('date', null)
+      this.set('date', null),
+      this.set('lotphoto', null)
     },
     delete: function(lot) {
       if (confirm("Are you sure?")) {
--- models/lot.js
+++ models/lot.js
@@ -1,6 +1,6 @@
 Auction.Lot = DS.Model.extend({
   title: DS.attr(),
   date: DS.attr(),
-  photo: DS.attr(),
+  lotphoto: DS.attr(),
   products: DS.hasMany('product', {async: true})
 });
